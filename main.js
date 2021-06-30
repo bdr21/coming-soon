6ce92f99482ecd44e983e72f47d01f6b58f4d306
bdr21 <omarbdr1999@gmail.com>
2021-06-30 17:10:13 +0100

    z

---
+++ main.js
@@ -0,0 +1,10 @@
+// const ficb = document.querySelectorAll(".focus-in-contract-bck");
+// setTimeout(() => {
+//   ficb.forEach((e) => {
+//     e.classList.toggle("animate__animated");
+//     e.classList.toggle("animate__flash");
+//     // e.classList.toggle("animate__slower");
+//     e.classList.toggle("animate__repeat-3");
+//     e.classList.toggle("focus-in-contract-bck");
+//   });
+// }, 2000);
